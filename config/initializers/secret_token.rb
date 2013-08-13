f66bc4ba124e2c07eaf51abcc878ac9d67bff83c
Matt Yeh <dev.mtyeh411@gmail.com>
2013-08-13 16:13:44 -0400

    initial commit with test env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GtfsRealtime::Application.config.secret_key_base = '299c52001cd332baa93b9d5d848261285cec35b2edae7abd4d9d18e7af2884900fd34e0f25245cb09704b4a33d900df115d01c445c01cd47747cb2a766ca5e28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GtfsRealtime::Application.config.session_store :cookie_store, key: '_gtfs_realtime_session'
