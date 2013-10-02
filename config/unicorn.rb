033f860a35eb1b2b78df7cf33cd675a2422e1a90
Matt Yeh <dev.mtyeh411@gmail.com>
2013-10-01 23:23:43 -0400

    configure for production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,8 @@
-if ENV['RAILS_ENV']=='production'
-  working_directory "/var/www/gtfs_realtime_viz/current"
-  pid "/var/www/gtfs_realtime_viz/current/tmp/pids/unicorn.pid"
-  stderr_path "/var/www/gtfs_realtime_viz/current/log/unicorn.log"
-  stdout_path "/var/www/gtfs_realtime_viz/current/log/unicorn.log"
+working_directory "/var/www/gtfs_realtime_viz/current"
+pid "/var/www/gtfs_realtime_viz/current/tmp/pids/unicorn.pid"
+stderr_path "/var/www/gtfs_realtime_viz/current/log/unicorn.log"
+stdout_path "/var/www/gtfs_realtime_viz/current/log/unicorn.log"
 
-  listen "/var/www/gtfs_realtime_viz/current/tmp/unicorn.sock"
-  worker_processes 2
-  timeout 30
-#else 
-#  working_directory "/Users/myeh/Projects/mc_trn/gtfs_realtime_map"
-#  pid "/Users/myeh/Projects/mc_trn/gtfs_realtime_map/tmp/pids/unicorn.pid"
-#  stderr_path "/Users/myeh/Projects/mc_trn/gtfs_realtime_map/log/unicorn.log"
-#  stdout_path "/Users/myeh/Projects/mc_trn/gtfs_realtime_map/log/unicorn.log"
-#
-#  listen "/Users/myeh/Projects/mc_trn/gtfs_realtime_map/tmp/unicorn.sock"
-#  worker_processes 2
-#  timeout 30
-end
+listen "/var/www/gtfs_realtime_viz/current/tmp/unicorn.sock"
+worker_processes 2
+timeout 30
